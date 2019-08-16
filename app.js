48f08dbf868c42084124ead1388f268a05661ec4
sreepathysois <54054574+sreepathysois@users.noreply.github.com>
2019-08-16 20:40:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World! of cloud computing'))
+app.get('/', (req, res) => res.send('Hello World! of cloud computing and devops'))
 
 var server = app.listen(3000, () => {
     console.log("Listening on port " + server.address().port + "...");
